fa0948ea7b0ba5709e1cfc6c452289e736395cd5
Dan Spector <danno2423@gmail.com>
2019-05-22 19:45:49 -0400

    more comments and added test

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const readline = require('readline'); //gets user input
 const fetch = require('node-fetch'); //node implemenation of fetch()
 
-//async function that gets json
+//async function gets json promise object, and gets the view based on a selector
 const returnSelectedView = async (selector) => {
   try {
+    //returns a json promise object
     const response = await fetch('https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json');
     const json = await response.json(); 
-    console.log(json);
-
+  
+    //these are the views that will be returned
     const views = [];
 
     const getView = (obj) => {
+      //for each view in the object, check if it matches the selector. if it does, push it to the views array
       obj.subviews.forEach(() => {
         if (obj['class'] === selector || obj['classNames'] === selector || obj['identifier'] === selector) {
         views.push(obj);
       }
-        getView(this);
+        //if there is child subview within the current subview object, recursively call getView() with the child subView object
+        if (obj.subviews) {
+          getView(obj.subviews);
+        }
       });
     }
+    //runs getView for the first time
     getView(json);
+
+    //returns the array of all views
     return views;
 
   } catch (err) {
@@ -35,6 +43,6 @@ const io = readline.createInterface({
 //ask for input selector and execute returnSelectedView() based on the selector provided
 io.question(`What selector do you want?`, (selector) => {
   returnSelectedView(selector);
-  readline.close()
+  io.close()
 })
 
