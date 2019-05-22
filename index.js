75e269cec250e7ede00cc52703abaf1dcd8383b6
Dan Spector <danno2423@gmail.com>
2019-05-22 19:17:50 -0400

    first crack at this

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const readline = require('readline'); //gets user input
 const fetch = require('node-fetch'); //node implemenation of fetch()
 
-//create io
-readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
 //async function that gets json
-const getJson = async () => {
+const returnSelectedView = async (selector) => {
   try {
     const response = await fetch('https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json');
     const json = await response.json(); 
     console.log(json);
-    
 
+    const views = [];
 
-
-
+    const getView = (obj) => {
+      obj.subviews.forEach(() => {
+        if (obj['class'] === selector || obj['classNames'] === selector || obj['identifier'] === selector) {
+        views.push(obj);
+      }
+        getView(this);
+      });
+    }
+    getView(json);
+    return views;
 
   } catch (err) {
     console.log(err);
   }
 }
 
-getJson();
+//create io
+readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-
-// readline.question(`What selector do you want?`, (selector) => {
-//   readline.close()
-// })
+//ask for input selector and execute returnSelectedView() based on the selector provided
+readline.question(`What selector do you want?`, (selector) => {
+  returnSelectedView(selector);
+  readline.close()
+})
 
