34244283e886ae4b0f3d397196b49890ae1f6682
Dan Spector <danno2423@gmail.com>
2019-05-22 18:17:52 -0400

    made gitignore

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const readline = require('readline'); //gets user input
 const fetch = require('node-fetch'); //node implemenation of fetch()
 
-//this function 
+//create io
+readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+//async function that gets json
 const getJson = async () => {
   try {
     const response = await fetch('https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json');
     const json = await response.json(); 
     console.log(json);
+    
+
+
+
+
+
   } catch (err) {
     console.log(err);
   }
@@ -14,11 +26,6 @@ const getJson = async () => {
 
 getJson();
 
-
-// readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// })
 
 // readline.question(`What selector do you want?`, (selector) => {
 //   readline.close()
