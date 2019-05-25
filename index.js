b6c856dfe761a02b96c49ae1c4244921eb77c0e2
Dan Spector <danno2423@gmail.com>
2019-05-25 12:44:03 -0400

    update with more work

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
 const readline = require('readline'); //gets user input
-const fetch = require('node-fetch'); //node implemenation of fetch()
+const fetch = require('node-fetch'); //node implemenation of fetch api
 
 //async function gets json promise object, and gets the view based on a selector
 const returnSelectedView = async (selector) => {
   try {
-
-    //this function is responsible for recursing down the tree and returning the views
-    const getView = (subviews) => {
-      // these are the views that will be returned
-      const views = [];
-      //iterate through each subview in the subviews array . 
-      subviews.forEach((subview) => {
-        // check if it matches the selector
-        if (subview.class === selector || subview.classNames === selector || subview.identifier === selector) {
-        // if the subview matches the selector, push it to the views array  
-        views.push(subview);
-         //if there is child subview within the current subview object, recursively call getView() with the child subView object
-         if (subview.subviews) {
-          getView(subview.subviews);
-        }
-      }
-        return views;
-      });
-    }
-
     //returns a json promise object
     const response = await fetch('https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json');
     const json = await response.json(); 
-    //runs getView for the first time
-    await getView(json.subviews);
+
+    //initialize array of views that will be returned
+    const views = [];
+
+    //determine what type of selector the user wants
+    let selectorType;
+    if (selector[0] === '.') {
+      selectorType = 'className';
+    } else if (selector[0] === '#') {
+      selectorType = 'identifier';
+    } else {
+      selectorType = 'class';
+    }
+
+    //selector logic - determines if the program will return subview based on the selector provided
+    const selectSubView = (subview) => {
+      switch (selectorType) {
+        case 'class':
+          if (subview.class && subview.class === selector) {
+            views.push(subview);
+          }
+        break;
+  
+        case 'className': 
+          if (subview.classNames && subview.classNames.includes(selector.slice(1))) {
+            views.push(subview);
+          }
+        break;
+  
+        case 'identifier':
+          if (subview.identifier && subview.identifier === selector.slice(1)) {
+            views.push(subview);
+          }
+        break;
+      }
+    }
+    
+    const iterateViews = (currentView) => {
+      if (currentView.subviews) {
+        currentView.subviews.forEach(subview => {
+          selectSubView(subview);
+          iterateViews(subview);
+        });
+      }
+    }
+
+    //run getView for the first time with the JSON object
+    iterateViews(json);
+
+    //return the views to the user
+    return views;
+
 
   } catch (err) {
     console.log(err);
   }
 }
 
-returnSelectedView('StackView');
+//create io
+const io = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-// //create io
-// const io = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// })
-
-// //ask for input selector and execute returnSelectedView() based on the selector provided
-// io.question(`What selector do you want?`, (selector) => {
-//   returnSelectedView(selector);
-//   io.close()
-// })
+//ask for input selector and execute returnSelectedView() based on the selector provided
+io.question(`What selector do you want?`, (selector) => {
+  returnSelectedView(selector);
+  io.close()
+})
 
