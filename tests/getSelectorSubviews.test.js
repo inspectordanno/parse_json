1b48f839f51fc5ea1623f466062b9d656f9b2748
Dan Spector <danno2423@gmail.com>
2019-05-27 21:35:49 -0400

    improved view tests

--- tests/getSelectorSubviews.test.js
+++ tests/getSelectorSubviews.test.js
@@ -2,21 +2,24 @@ import { getJSON, getSelectorSubviews } from '../index';
 
 const fixture = 'https://raw.githubusercontent.com/inspectordanno/parse_json/master/tests/fixture.json';
 
-it('should return two views with a class of Input', async () => {
+it('should detect two views with a class of Input', async () => {
   const json = await getJSON(fixture);
   const views = getSelectorSubviews('Input', 'class', json);
+  views.forEach(view => expect(view).toHaveProperty('class', 'Input'));
   expect(views).toHaveLength(2);
 });
 
-it('should return three views with a className of container', async () => {
+it('should detect three views with a className of container', async () => {
   const json = await getJSON(fixture);
   const views = getSelectorSubviews('.container', 'className', json);
+  views.forEach(view => expect(view.classNames).toContain('container'));
   expect(views).toHaveLength(3);
 });
 
-it('should return one view with an identifier of videoMode', async () => {
+it('should detect one view with an identifier of videoMode', async () => {
   const json = await getJSON(fixture);
   const views = getSelectorSubviews('#videoMode', 'identifier', json);
+  views.forEach(view => expect(view).toHaveProperty('identifier', 'videoMode'));
   expect(views).toHaveLength(1);
 });
 
